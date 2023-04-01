b76b2ae403bf3645817feafeba3526dcc6090dc5
Thomas Lawless <lawlessptom@gmail.com>
2023-04-01 12:30:15 -0400

    Initial Commit

---
+++ Task Manager/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Task Manager
+//
+//  Created by Thomas Lawless III on 4/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Thomas Lawless III on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
